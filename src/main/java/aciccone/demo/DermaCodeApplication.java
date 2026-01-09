3a097401a1606cc145a30744059cf0493c9280fb
alessandraciccone <alessandraciccone@icloud.com>
2026-01-09 12:35:06 +0100

    Initial commit

---
+++ src/main/java/aciccone/demo/DermaCodeApplication.java
@@ -0,0 +1,13 @@
+package aciccone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermaCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DermaCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aciccone/demo/DermaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package aciccone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermaCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
